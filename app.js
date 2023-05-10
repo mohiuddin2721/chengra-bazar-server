a0b6001f7d462e391c039e7790522cf0c83a3a5d
mohiuddin2721 <mohiuddinpatwary27@gmail.com>
2023-05-11 01:42:09 +0600

    data inserted in database

--- app.js
+++ app.js
@@ -61,6 +61,15 @@ const productSchema = mongoose.Schema({
     minLength: [3, 'category must be at least 3 characters.'],
     maxLength: [100, 'category is too large. It must be at least 100 characters'],
   },
+  color: {
+    type: String,
+  },
+  brand: {
+    type: String,
+  },
+  ratting: {
+    type: Number,
+  }
 }, {
   timestamps: true
 })
@@ -71,5 +80,32 @@ const Product = mongoose.model('Product', productSchema)
 app.get('/', (req, res) => {
   res.send('Chengra_Bazar route is working! YaY');
 });
+
+// posting to database
+app.post('/api/v1/product', async (req, res, next) => {
+  try {
+    // data save() or create()
+    // const result = await Product.create(req.body);
+    // save() is the best method to insert data in database, because we can take any action before the data insert;
+    // instance creation -> Do something -> save()
+    const product = new Product(req.body);
+    if (product.quantity === 0) {
+      product.status = 'out-of-stock';
+    }
+    const result = await product.save();
+
+    res.status(200).json({
+      status: 'success',
+      message: 'Successfully data inserted',
+      data: result,
+    })
+  } catch (error) {
+    res.status(400).json({
+      status: 'fail',
+      message: 'Data is not inserted',
+      error: error.message,
+    })
+  }
+})
 
 module.exports = app;
