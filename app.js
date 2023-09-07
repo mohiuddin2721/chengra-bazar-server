dc42009071f8585053657fcf0b4ed5c130675590
mohiuddin2721 <mohiuddinpatwary27@gmail.com>
2023-09-07 22:49:23 +0400

    finaly done

--- app.js
+++ app.js
@@ -25,18 +25,12 @@ app.use('/api/v1/addCart', addCartsRoute)
 app.use('/api/v1/address', shippingAddress)
 app.use('/api/v1/payment', paymentRoute)
 
-app.get("/products", async (req, res) => {
-  const product = await Product.find()
-  console.log(product)
-  res.send(product)
-})
+
 
 app.get('/', (req, res) => {
   res.send('Chengra_Bazar route is working! YaY');
 });
-app.get('/test',(req,res)=>{
-  res.send('test list')
-})
+
 
 
 module.exports = app;
